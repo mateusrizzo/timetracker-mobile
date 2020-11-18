f572f8d0a33e87200eb2f716398a0e726cd4d306
Mateus Rizzo <mateusrizzo13@gmail.com>
2020-11-18 16:20:33 -0300

    styled sign up page

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,10 +1,98 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useState} from 'react';
+import {useNavigation} from '@react-navigation/native';
+import {
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-export default function SignUp() {
+import styles from './styles';
+
+// import api from '../../network/api';
+
+export default function Login() {
+  const [name, setName] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+
+  const navigation = useNavigation();
+
+  function handleSignUp() {
+    console.log(name);
+    console.log(email);
+    console.log(password);
+  }
   return (
-    <>
-      <Text style={{fontSize: 16, alignSelf: 'center'}}>Sign Up</Text>
-    </>
+    <ScrollView keyboardShouldPersistTaps="never">
+      <View style={styles.container}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>Sign Up</Text>
+        </View>
+        <View style={styles.form}>
+          <View style={styles.inputContainer}>
+            <Icon name="person" size={14} color="#B0B0C3" style={styles.icon} />
+            <TextInput
+              style={styles.input}
+              autoCapitalize="none"
+              autoCompleteType="name"
+              placeholder="Name"
+              keyboardType="default"
+              returnKeyType="next"
+              returnKeyLabel="next"
+              value={name}
+              onChangeText={setName}
+            />
+          </View>
+          <View style={[styles.inputContainer, styles.bottomInput]}>
+            <Icon name="mail" size={14} color="#B0B0C3" style={styles.icon} />
+            <TextInput
+              style={styles.input}
+              autoCapitalize="none"
+              autoCompleteType="email"
+              placeholder="Email"
+              keyboardType="email-address"
+              returnKeyType="next"
+              returnKeyLabel="next"
+              value={email}
+              onChangeText={setEmail}
+            />
+          </View>
+          <View style={[styles.inputContainer, styles.bottomInput]}>
+            <Icon name="lock" size={14} color="#B0B0C3" style={styles.icon} />
+            <TextInput
+              style={styles.input}
+              placeholder="Password"
+              secureTextEntry
+              autoCompleteType="off"
+              value={password}
+              onChangeText={setPassword}
+            />
+          </View>
+          <View style={[styles.inputContainer, styles.bottomInput]}>
+            <Icon name="lock" size={14} color="#B0B0C3" style={styles.icon} />
+            <TextInput
+              style={styles.input}
+              placeholder="Confirm Password"
+              secureTextEntry
+              autoCompleteType="off"
+              value={password}
+              onChangeText={setPassword}
+            />
+          </View>
+        </View>
+        <TouchableOpacity style={styles.signUpButton} onPress={handleSignUp}>
+          <Text style={styles.buttonText}>Create Account</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.loginContainer}>
+        <Text style={styles.loginText}>Already have an account?</Text>
+        <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+          <Text style={styles.loginButtonText}> Login in</Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
   );
 }
---
+++ src/pages/SignUp/styles.js
@@ -0,0 +1,72 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    flex: 1,
+  },
+  titleContainer: {
+    justifyContent: 'center',
+    marginTop: 60,
+  },
+  title: {
+    fontSize: 22,
+    fontFamily: 'Montserrat-SemiBold',
+    textAlign: 'center',
+  },
+  image: {
+    marginTop: 51,
+  },
+  form: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 90,
+  },
+  inputContainer: {
+    width: 315,
+    height: 60,
+    flexDirection: 'row',
+    backgroundColor: '#BBDEFB',
+    alignItems: 'center',
+  },
+  icon: {
+    marginLeft: 15,
+  },
+  input: {
+    flex: 1,
+    fontFamily: 'Montserrat-Medium',
+    backgroundColor: '#BBDEFB',
+  },
+  bottomInput: {
+    marginTop: 20,
+  },
+  signUpButton: {
+    marginTop: 50,
+    backgroundColor: '#00897B',
+    width: 315,
+    height: 60,
+    justifyContent: 'center',
+  },
+  buttonText: {
+    color: '#Fdfafa',
+    fontSize: 18,
+    fontFamily: 'Montserrat-Medium',
+    alignSelf: 'center',
+  },
+  loginContainer: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 90,
+  },
+  loginText: {
+    fontFamily: 'Montserrat-Medium',
+    fontSize: 16,
+  },
+  loginButtonText: {
+    fontFamily: 'Montserrat-Bold',
+    fontSize: 16,
+    textDecorationLine: 'underline',
+    color: '#00897B',
+  },
+});
